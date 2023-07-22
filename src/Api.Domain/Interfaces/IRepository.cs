b26b6b61ae3935b0b8fe755ab91c9b326ee7d0c3
Danilo Silva <danilo.silva@msn.com>
2023-07-22 10:36:43 -0300

    feat: Adição da Interface Base para os Repositórios

---
+++ src/Api.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+using Api.Domain.Entities;
+
+namespace Api.Domain.Interfaces;
+
+public interface IRepository<T> where T : BaseEntity
+{
+    Task<T> InserirAsync(T item);
+    Task<T> AtualizarAsync(T item);
+    Task<bool> ExcluirAsync(Guid id);
+    Task<T> SelecionarAsync(Guid id);
+    Task<IEnumerable<T>> SelecionarAsync();
+}
